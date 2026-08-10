9e32cdc4f32dd3cb2cbff6f87b8d930edbd855d2
M-Hegab <mohamedtamerhegab5@gmail.com>
2026-08-10 14:51:45 +0300

    enhance log message for file processing count

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const incomingFolder: string = './incomingFiles';
 
-console.log(await countFiles(incomingFolder));
+console.log(await countFiles(incomingFolder), "files waiting for processing");
 
 app.listen(port, () => {
   console.log(`File Processing System listening on port ${port}`);
